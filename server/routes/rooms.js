4f15a11f8d474d75280ff04ed349e72da2fb97be
alihassan-here <alihassanuog50@gmail.com>
2022-05-29 13:00:25 +0500

    room CRUD

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,9 +1,22 @@
 import express from 'express';
 const router = express.Router();
+import { createRoom, updateRoom, deleteRoom, getRoom, getRooms } from '../controllers/room.js';
+import { verifyAdmin } from "../utils/verifyToken.js";
 
+//CREATE
+router.post('/:hotelId', verifyAdmin, createRoom);
 
-router.get('/', (req, res) => {
-    res.send("this is rooms route!");
-});
+//UPDATE
+router.put('/:id', verifyAdmin, updateRoom);
+
+//DELETE
+router.delete('/:id/:hotelId', verifyAdmin, deleteRoom);
+
+//GET ROOM
+router.get('/:id', getRoom);
+
+//GET ALL ROOMS
+router.get('/', getRooms);
+
 
 export default router;
